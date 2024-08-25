98f73471a84a4cff3a9ca3000433719feddd10b2
Venu Vihari <146341611+VenuVihari@users.noreply.github.com>
2024-08-25 12:20:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require("cors");
 //const PORT = process.env.PORT || 3000;
-
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
